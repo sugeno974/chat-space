8ce1c675c8c742fa71cffd68e7f7417a07a11ef4
sugeno974 <sugeno974@gmail.com>
2017-05-07 22:52:22 +0900

    create controller 'groups'

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,30 @@
 class GroupsController < ApplicationController
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(create_params)
+    if @group.save
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    group = Group.find(params[:id])
+    group.update(create_params)
+  end
+
+  private
+
+  def create_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
+
 end
